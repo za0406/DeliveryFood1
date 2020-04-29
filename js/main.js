5d04a041508ac0e9f6b5e7608be3c847e0f34f9c
za0406 <64550068+za0406@users.noreply.github.com>
2020-04-29 22:46:32 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const CartButton = document.querySelector('#cart-button');
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+CartButton.addEventListener('click', toggleModal);
+
+close.addEventListener('click', toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+new WOW().init()
